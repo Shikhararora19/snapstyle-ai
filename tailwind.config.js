11c304ed72e460516760f311631f861a87172589
Shikhar Arora <shikhar3@ualberta.ca>
2025-01-01 01:58:48 -0700

    Fix: Applying styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./public/index.html", // Include HTML if used
+  ],
   theme: {
     extend: {},
   },
