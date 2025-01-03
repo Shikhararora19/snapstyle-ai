7156b16e80650085062f3796b5f90f13dd4d7065
Shikhar Arora <shikhar3@ualberta.ca>
2025-01-03 13:29:41 -0700

    Update StyleCard.tsx

--- src/components/StyleCard.tsx
+++ src/components/StyleCard.tsx
@@ -109,7 +109,7 @@ const StyleCard: React.FC<StyleCardProps> = ({ style }) => {
         <img
           src={image}
           alt={style.name}
-          className="w-full h-60 object-cover mb-4 rounded"
+          className="w-full h-2/3 object-cover mb-4 rounded"
         />
       ) : (
         <div className="w-full h-60 bg-gray-200 flex items-center justify-center rounded mb-4">
